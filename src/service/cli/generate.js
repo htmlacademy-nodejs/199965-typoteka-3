6fee6da6f16a0872f4c480c7aac1d7ca7eca9033
Elena Efimova <ees.elena@gmail.com>
2020-10-18 17:49:02 +0300

    mod2-task2 ready

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,17 +1,16 @@
 "use strict";
 
 const fs = require(`fs`).promises;
+const path = require(`path`);
 const chalk = require(`chalk`);
 const {getRandomInt, shuffle} = require(`../../utils`);
 
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
 
-const {
-  titles,
-  announces,
-  categories,
-} = require(`./constants/generateData.json`);
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 const getCurrentDateTime = () => {
   const date = new Date().toJSON().slice(0, 10);
@@ -19,28 +18,41 @@ const getCurrentDateTime = () => {
   return `${date} ${time}`;
 };
 
-const generateOffers = (count) =>
+const generateOffers = (count, titles, categories, sentences) =>
   Array(count)
     .fill({})
     .map(() => ({
-      title: titles[getRandomInt(0, titles.length - 1)],
-      announce: shuffle(announces).slice(1, 5).join(` `),
-      fullText: shuffle(announces)
-        .slice(1, announces.length - 1)
+      category: shuffle(categories).slice(0, getRandomInt(1, categories.length - 1)),
+      description: shuffle(sentences).slice(1, 5).join(` `),
+      fullText: shuffle(sentences)
+        .slice(1, sentences.length - 1)
         .join(` `),
-      createdDate: getCurrentDateTime(),
-      category: shuffle(categories).slice(
-          0,
-          getRandomInt(1, categories.length - 1),
-      ),
+      title: titles[getRandomInt(1, titles.length - 1)],
+      createdDate: getCurrentDateTime()
     }));
+
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(path.resolve(__dirname, filePath), `utf8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+
+
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer));
+    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
 
     try {
       await fs.writeFile(FILE_NAME, content);
